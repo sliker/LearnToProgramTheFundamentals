22dd3f8948dafe9d7229a448e75a9ee19f19187d
David Velásquez <zero.blend@gmail.com>
2013-09-24 17:16:27 -0500

    n

---
+++ exercices.py
@@ -0,0 +1,79 @@
+def merge(L):
+    merged = []
+    for i in range(0, len(L), 3):
+        merged.append(L[i] + L[i + 1] + L[i + 2])
+    return merged
+
+def shift_right(L):
+    ''' (list) -> NoneType
+
+    Shift each item in L one position to the right
+    and shift the last item to the first position.
+
+    Precondition: len(L) >= 1
+    '''
+
+    last_item = L[-1]
+
+    for i in range(len(L) - 1):
+        L[i] = L[i + 1]
+
+    L[0] = last_item
+
+def make_pairs(list1, list2):
+    ''' (list of str, list of int) -> list of [str, int] list
+    
+    Return a new list in which each item is a 2-item list with the string from the
+    corresponding position of list1 and the int from the corresponding position of list2.
+    
+    Precondition: len(list1) == len(list2)
+    
+    >>> make_pairs(['A', 'B', 'C'], [1, 2, 3])
+    [['A', 1], ['B', 2], ['C', 3]]
+    '''
+    
+    pairs = []
+    
+    for i in range(len(list1)):
+        inner_list = []
+        inner_list.append(list1[i])
+        inner_list.append(list2[i])
+        pairs.append(inner_list)
+        
+    return pairs
+
+
+def contains(value, lst):
+    """ (object, list of list) -> bool
+
+    Return whether value is an element of one of the nested lists in lst.
+
+    >>> contains('moogah', [[70, 'blue'], [1.24, 90, 'moogah'], [80, 100]])
+    True
+    """
+
+    found = False  # We have not yet found value in the list.
+
+    for item in lst:
+        if value == item:
+            value = True
+
+    return found
+
+def lines_startswith(file, letter):
+    """ (file open for reading, str) -> list of str
+
+    Return the list of lines from file that begin with letter. The lines should have the
+    newline removed.
+
+    Precondition: len(letter) == 1
+    """
+
+    matches = []
+
+    # CODE MISSING HERE
+    for line in file:
+        if line.startswith(letter):
+            matches.append(line.rstrip('\n'))
+
+    return matches
